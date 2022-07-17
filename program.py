cbba42aaf96a6d510b8afa2b704f4a5452615194
chegotova <103313397+chegotova@users.noreply.github.com>
2022-07-17 21:07:15 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -1,9 +1,18 @@
-# Реализуйте выдачу случайного числа (или алгоритм перемешивания списка)
+#Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
-x = int(input('введите число x'))
-y = int(input('введите число y'))
-z = int(input('введите число z'))
-z = x ^ y**2
-print('x = ', x)
-print('y = ', y)
-print('z = ', z)
+#*Пример:*
+
+#- [1.1, 1.2, 3.1, 5.561, 10.01] => 0.56 или 56
+
+import random
+import math
+ 
+number = 123.345
+split_number = list(str(number).split('.'))
+print(split_number)
+ 
+matrix = [random.uniform (2, 6) for item in range(4)]
+for x in range(len(matrix)):
+   print  (math.modf(x))
+    if 
+ 
